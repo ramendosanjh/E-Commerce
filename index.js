59145008ab48456ddb4615e6a4adaaf4226502d6
Ramen <134460692+ramendosanjh@users.noreply.github.com>
2023-11-06 14:14:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-// import models
 const Product = require('./models/Product');
 const Category = require('./models/Category');
 const Tag = require('./models/Tag');
 const ProductTag = require('./models/ProductTag');
 
-// Products belongsTo Categmpde ory
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
-// Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE', // This sets the behavior on deletion (you can customize it)
+  onDelete: 'CASCADE', 
 });
 
-// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
 });
 
-// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
