dd68d0e46ba5fc57c695ae4f97d36e8cf1de150f
Ramen Kaur <ramendosanjh@outlook.com>
2023-11-05 19:19:50 -0500

    commit

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
 // GET all categories with their associated products
 router.get('/', async (req, res) => {
   try {
